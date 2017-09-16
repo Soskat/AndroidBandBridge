9ec011c139dc1751833751297e1cf938bd161e6c
Soskat <soskat@outlook.com>
2017-09-16 21:58:08 +0200

    Added Communication namespace from BandBridge project

---
+++ AndroidBandBridge/BandBridge/Communication/Data/Message.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace Communication.Data
+{
+    /// <summary>
+    /// Class that represents a message structure used in communication with BandBridge server.
+    /// </summary>
+    [DataContract]
+    public class Message
+    {
+        #region Static fields
+        /// <summary>
+        /// An array with types known by DataContractSerializer.
+        /// </summary>
+        public static Type[] SerializedTypesSet = { typeof(SensorData), typeof(SensorData[]) };
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Message command type code.
+        /// </summary>
+        [DataMember]
+        public MessageCode Code { get; set; }
+
+        /// <summary>
+        /// Message result object.
+        /// </summary>
+        [DataMember]
+        public object Result { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of class <see cref="Message"/>.
+        /// </summary>
+        /// <param name="code">Message command type code</param>
+        /// <param name="result">Message result object</param>
+        public Message(MessageCode code, object result)
+        {
+            Code = code;
+            Result = result;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Serializes <see cref="Message"/> object to a byte array.
+        /// </summary>
+        /// <param name="message">Message to serialize</param>
+        /// <returns>Serialized message in form of an array of bytes</returns>
+        public static byte[] Serialize(Message message)
+        {
+            byte[] data = null;
+            using (MemoryStream stream = new MemoryStream())
+            using (XmlDictionaryWriter writer = XmlDictionaryWriter.CreateBinaryWriter(stream))
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(Message), SerializedTypesSet);
+                serializer.WriteObject(writer, message);
+                writer.Flush();
+                data = stream.ToArray();
+            }
+            return data;
+        }
+
+        /// <summary>
+        /// Deserializes byte array as <see cref="Message"/> object.
+        /// </summary>
+        /// <param name="data">Array of bytes with serialized message</param>
+        /// <returns>Deserialized message</returns>
+        public static Message Deserialize(byte[] data)
+        {
+            Message message = null;
+            using (MemoryStream stream = new MemoryStream(data))
+            using (XmlDictionaryReader reader = XmlDictionaryReader.CreateBinaryReader(stream, XmlDictionaryReaderQuotas.Max))
+            {
+                DataContractSerializer deserializer = new DataContractSerializer(typeof(Message), SerializedTypesSet);
+                message = (Message)deserializer.ReadObject(reader);
+            }
+            return message;
+        }
+
+        /// <summary>
+        /// Writes Message object in form: 'Message: [Code][Result] -> Result.ToString()'.
+        /// </summary>
+        /// <returns>String version of the <see cref="Message"/> object</returns>
+        public override string ToString()
+        {
+            if (Result != null)
+                return string.Format("Message: [{0}][{1}] -> {2}", Code, Result, Result.ToString());
+            else
+                return string.Format("Message: [{0}][{1}]", Code, Result);
+        }
+        #endregion
+    }
+}
---
+++ AndroidBandBridge/BandBridge/Data/CircularBuffer.cs
@@ -0,0 +1,133 @@
+﻿using System.Text;
+
+namespace BandBridge.Data
+{
+    /// <summary>
+    /// Circular buffer. In case of the end of free space, old data will be ovewritten.
+    /// </summary>
+    public class CircularBuffer
+    {
+        #region Fields
+        /// <summary>
+        /// Table represents the buffer.
+        /// </summary>
+        private int[] buffer;
+
+        /// <summary>
+        /// The index of currently active buffer element.
+        /// </summary>
+        private int iterator;
+
+        /// <summary>
+        /// The capacity of the buffer.
+        /// </summary>
+        private int capacity;
+
+        /// <summary>
+        /// Is the buffer full?
+        /// </summary>
+        private bool isFull;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// The capacity of the buffer.
+        /// </summary>
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+        /// <summary>
+        /// Is the buffer full?
+        /// </summary>
+        public bool IsFull
+        {
+            get { return isFull; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of class <see cref="CircularBuffer"/>.
+        /// </summary>
+        /// <param name="size">Size of the buffer</param>
+        public CircularBuffer(int size)
+        {
+            capacity = size;
+            buffer = new int[capacity];
+            iterator = 0;
+            isFull = false;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Adds new value to the buffer.
+        /// </summary>
+        /// <param name="obj">Value to add</param>
+        public void Add(int obj)
+        {
+            buffer[iterator] = obj;
+            iterator = ++iterator % buffer.Length;
+            if (iterator == 0) isFull = true;
+        }
+
+        /// <summary>
+        /// Clears the buffer.
+        /// </summary>
+        public void Clear()
+        {
+            buffer = new int[capacity];
+            iterator = 0;
+            isFull = false;
+        }
+
+        /// <summary>
+        /// Resizes the buffer.
+        /// </summary>
+        /// <param name="newSize">New size of the buffer</param>
+        public void Resize(int newSize)
+        {
+            capacity = newSize;
+            Clear();
+        }
+
+        /// <summary>
+        /// Returns the average of values currently stored in buffer.
+        /// </summary>
+        /// <returns>The average of values stored in buffer</returns>
+        public int GetAverage()
+        {
+            int sum = 0, itemsCount = 0;
+            foreach (int obj in buffer)
+            {
+                if (obj > 0)
+                {
+                    sum += obj;
+                    itemsCount++;
+                }
+            }
+            if (itemsCount > 0)
+                return sum / itemsCount;
+            else
+                return 0;
+        }
+
+        /// <summary>
+        /// Writes all buffer elements in form of: [a1 | a2 | ... an | ]
+        /// </summary>
+        /// <returns>CircularBuffer in string format</returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder("[ ");
+            foreach (int item in buffer)
+            {
+                sb.Append(item + " | ");
+            }
+            sb.Append("]");
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
